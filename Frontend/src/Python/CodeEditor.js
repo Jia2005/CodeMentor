68178321168cb18c0aa2370021e2a02099a2b693
Jia Harisinghani <jia2.harisinghani@gmail.com>
2025-08-03 14:27:25 +0530

    height adjustment

--- Frontend/src/Python/CodeEditor.js
+++ Frontend/src/Python/CodeEditor.js
@@ -4,7 +4,7 @@ function CodeEditor({ code, setCode }) {
   return (
     <Editor
       // The editor will now take the height of its container
-      height="500px"
+      height="350px"
       corner="rounded-xl"
       defaultLanguage="python"
       value={code}
