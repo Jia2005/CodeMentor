ae474e39d062dc9521e0773df261ad89127a3221
Jia Harisinghani <jia2.harisinghani@gmail.com>
2025-08-04 00:40:25 +0530

    handling user input using server.js

--- Backend/server.js
+++ Backend/server.js
@@ -13,26 +13,23 @@ const PORT = process.env.PORT || 3001;
 wss.on('connection', ws => {
   console.log('Client connected');
 
-  // Use '-u' for unbuffered output
+  // Use '-u' for unbuffered output from the python script
   const pythonProcess = spawn('python', ['-u']);
 
-  // Stream stdout from Python to the WebSocket client
   pythonProcess.stdout.on('data', data => {
     ws.send(JSON.stringify({ type: 'stdout', data: data.toString() }));
   });
 
-  // Stream stderr from Python to the WebSocket client
   pythonProcess.stderr.on('data', data => {
     ws.send(JSON.stringify({ type: 'stderr', data: data.toString() }));
   });
 
-  // When the Python process finishes, close the WebSocket connection.
+  // When the Python process finishes, close the connection
   pythonProcess.on('exit', (code) => {
     console.log(`Python process exited with code ${code}`);
     ws.close();
   });
 
-  // Handle messages from the client (code to execute)
   ws.on('message', message => {
     try {
       const parsedMessage = JSON.parse(message);
@@ -40,11 +37,10 @@ wss.on('connection', ws => {
         pythonProcess.stdin.write(parsedMessage.code + '\n');
       }
       if (parsedMessage.data) {
-        pythonProcess.stdin.write(parsedMessage.data);
+        // Add a newline to the user input to satisfy Python's input()
+        pythonProcess.stdin.write(parsedMessage.data + '\n');
       }
-      // *** THIS IS THE FIX ***
-      // Signal that no more data will be written to stdin.
-      // This "un-sticks" the Python input() function.
+      // Signal that no more input is coming
       pythonProcess.stdin.end();
 
     } catch (e) {
@@ -52,10 +48,8 @@ wss.on('connection', ws => {
     }
   });
 
-  // Handle client disconnection
   ws.on('close', () => {
     console.log('Client disconnected');
-    // Ensure the Python process is terminated if the client disconnects.
     pythonProcess.kill();
   });
 
